aa3d531f1d3ffe84546dadb39778a364d7df5941
MrMks <i0991958@163.com>
2022-03-03 20:28:21 +0800

    0.0.9-build14
    * 为EntityCustomNpc脚本的报错信息添加报错npc名称

--- src/main/java/com/github/mrmks/mc/csf/visitor/DataScriptClassVisitor.java
+++ src/main/java/com/github/mrmks/mc/csf/visitor/DataScriptClassVisitor.java
@@ -22,15 +22,25 @@ public class DataScriptClassVisitor extends ClassVisitor {
 
 
     private static class MethodVisitorImpl extends MethodVisitor {
-        private boolean f = false;
+        private boolean f1 = false, f2 = false;
         public MethodVisitorImpl(MethodVisitor mv) {
             super(ASM5, mv);
         }
 
         @Override
         public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
-            if (!f && "com/google/common/base/MoreObjects$ToStringHelper".equals(owner) && "toString".equals(name)) {
-                f = true;
+            if (!f2 && "com/google/common/base/MoreObjects".equals(owner) && "toStringHelper".equals(name)) {
+                super.visitMethodInsn(opcode, owner, name, desc, itf);
+                f2 = true;
+                mv.visitLdcInsn("name");
+                mv.visitVarInsn(ALOAD, 0);
+                mv.visitFieldInsn(GETFIELD, "noppes/npcs/entity/data/DataScript", "npc", "Lnoppes/npcs/entity/EntityNPCInterface;");
+                mv.visitFieldInsn(GETFIELD, "noppes/npcs/entity/EntityNPCInterface", "display", "Lnoppes/npcs/entity/data/DataDisplay;");
+                mv.visitMethodInsn(INVOKEVIRTUAL, "noppes/npcs/entity/data/DataDisplay", "getName", "()Ljava/lang/String;", false);
+                mv.visitMethodInsn(INVOKEVIRTUAL, "com/google/common/base/MoreObjects$ToStringHelper", "add", "(Ljava/lang/String;Ljava/lang/Object;)Lcom/google/common/base/MoreObjects$ToStringHelper;", false);
+            }
+            else if (!f1 && "com/google/common/base/MoreObjects$ToStringHelper".equals(owner) && "toString".equals(name)) {
+                f1 = true;
                 boolean d = FMLLaunchHandler.isDeobfuscatedEnvironment();
                 mv.visitLdcInsn("world");
                 mv.visitVarInsn(ALOAD, 0);
@@ -46,8 +56,9 @@ public class DataScriptClassVisitor extends ClassVisitor {
                 mv.visitFieldInsn(GETFIELD, "net/minecraft/world/World", d ? "provider" : "field_73011_w", "Lnet/minecraft/world/WorldProvider;");
                 mv.visitMethodInsn(INVOKEVIRTUAL, "net/minecraft/world/WorldProvider", "getDimension", "()I", false);
                 mv.visitMethodInsn(INVOKEVIRTUAL, "com/google/common/base/MoreObjects$ToStringHelper", "add", "(Ljava/lang/String;I)Lcom/google/common/base/MoreObjects$ToStringHelper;", false);
+                super.visitMethodInsn(opcode, owner, name, desc, itf);
             }
-            super.visitMethodInsn(opcode, owner, name, desc, itf);
+            else super.visitMethodInsn(opcode, owner, name, desc, itf);
         }
     }
 
